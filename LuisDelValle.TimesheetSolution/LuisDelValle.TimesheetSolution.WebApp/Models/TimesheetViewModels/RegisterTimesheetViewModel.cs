3824306d5feaf1135e93e3fee41a80be5e0b9c53
luisdvalle <jums.delvalle@gmail.com>
2018-06-01 23:18:42 +1000

    Adding properties and attributes to RegisterTimesheetVewModel

--- LuisDelValle.TimesheetSolution/LuisDelValle.TimesheetSolution.WebApp/Models/TimesheetViewModels/RegisterTimesheetViewModel.cs
+++ LuisDelValle.TimesheetSolution/LuisDelValle.TimesheetSolution.WebApp/Models/TimesheetViewModels/RegisterTimesheetViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace LuisDelValle.TimesheetSolution.WebApp.Models.TimesheetViewModels
 {
     public class RegisterTimesheetViewModel
     {
+        [Required, DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+        [Required, Range(0, 8)]
+        public int Hours { get; set; }
     }
 }
