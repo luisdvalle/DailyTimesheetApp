f8e1586242aa92899a8773eddf271a1ed3db9b60
luisdvalle <jums.delvalle@gmail.com>
2018-06-01 23:07:10 +1000

    Creating Interface and implementation of a RestClient

---
+++ LuisDelValle.TimesheetSolution/LuisDelValle.TimesheetSolution.WebApp/Services/IRestClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuisDelValle.TimesheetSolution.WebApp.Services
+{
+    public interface IRestClient<T>
+    {
+        string Host { get; set; }
+        string Path { get; set; }
+
+        Task<T> GetResponseAsync();
+    }
+}
---
+++ LuisDelValle.TimesheetSolution/LuisDelValle.TimesheetSolution.WebApp/Services/RestClient.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LuisDelValle.TimesheetSolution.WebApp.Services
+{
+    public class RestClient<T> : IRestClient<T> where T : class
+    {
+        public string Host { get; set; }
+        public string Path { get; set; }
+
+        private static HttpClient Client = new HttpClient();
+
+        public async Task<T> GetResponseAsync()
+        {
+            var stringResponse = await Client.GetStringAsync(Host + Path);
+
+            if (stringResponse != null)
+            {
+                T response = JsonConvert.DeserializeObject<T>(stringResponse);
+
+                if (response != null)
+                {
+                    return response;
+                }
+            }
+
+            return null;
+        }
+    }
+}
